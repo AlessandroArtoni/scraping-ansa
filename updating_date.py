92ac403b218e40f25a8a1d97f7be998c0675ddc5
AlessandroArtoni <alessandro1.artoni@mail.polimi.it>
2018-05-24 12:04:30 +0200

    adding ids to script

--- updating_date.py
+++ updating_date.py
@@ -10,15 +10,17 @@ import pymysql
 connection = pymysql.connect(host='localhost', port=3306, user='root', password='mamma93', db='mercurio',
                              charset='utf8mb4', cursorclass=pymysql.cursors.DictCursor)
 cur = connection.cursor(pymysql.cursors.DictCursor)
-sql = "SELECT date FROM articles_prova_ansa"
+sql = "SELECT id, date FROM articles_prova_ansa"
 cur.execute(sql)
 for row in cur:
-    update = ""
+    print (row)
+    '''
     try:
         with connection.cursor() as cursor:
             query = "INSERT INTO articles_prova_ansa (date_correct_type) VALUES (STR_TO_DATE('%s', " \
-                    "'%%Y/%%m/%%d %%h:%%i'))"
+                    "'%%Y-%%m-%%d %%h:%%i'))"
             cursor.execute(query, [row])
             connection.commit()
     except Exception, e:
         print("Can't insert date " + str(e))
+    '''
