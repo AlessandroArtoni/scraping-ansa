41a4ec9a47b8d6459d58922a72bb2b7ba4f6f2e4
Federico Amadelli <federico.amadelli@mail.polimi.it>
2018-05-24 22:17:01 +0200

    modified a date

--- mainScript.py
+++ mainScript.py
@@ -222,7 +222,7 @@ for company in list_of_companies:
                     day = date[:endDay]
                     month = date[endDay+1:endMonth]
                     year = date[endMonth+1:]
-                    date = year + "-" + month + "-" + day
+                    date = year + "/" + month + "/" + day
                     date = date.replace(" ", "")
 
                     if len(bodyArticle)<10:
@@ -230,7 +230,7 @@ for company in list_of_companies:
                     print nameCompany, linkedCompanies, ' ', count, ':', title, date, nomeAutore, link, bodyArticle
                     try:
                         with connection.cursor() as cursor:
-                            query = "INSERT INTO articles_finanza_com (date, newspaper, section, title, body, company, author, tagged_companies) VALUES (str_to_date('%s','%%Y-%%m-%%d'), %s, %s, %s, %s, %s, %s, %s)"
+                            query = "INSERT INTO articles_finanza_com (date, newspaper, section, title, body, company, author, tagged_companies) VALUES (str_to_date('%s','%%Y/%%m/%%d'), %s, %s, %s, %s, %s, %s, %s)"
                             cursor.execute(query, [date, "finanza.com", "economy", title, bodyArticle, nameCompany, nomeAutore, linkedCompanies])
                             connection.commit()
 
